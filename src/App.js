df0fe8546fb8256ab8b964ea54e22a349264fab4
meetmmpatel <meetmmpatel@gmail.com>
2019-12-29 20:17:42 -0500

    Updated Repo

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Customers from "./Customers";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router >
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Customer List with Details</h1>
+          </header>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <Redirect to="/customerlist" />}
+            />
+            <Route exact path="/customerlist" component={Customers} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
